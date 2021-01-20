675573ae27a90b8efa6e3673c86ede102f507b13
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-20 17:50:56 +0530

    WPF-56496 - custom recurring appointment sample added.

---
+++ SchedulerWPF/SchedulerWPF/Model/Meeting.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class Meeting
+    {
+        public ObservableCollection<DateTime> RecurrenceExceptions { get; set; } = new ObservableCollection<DateTime>();
+        public string EventName { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public object Id { get; set; }
+        public Brush BackgroundColor { get; set; }
+        public Brush ForegroundColor { get; set; }
+        public string RecurrenceRule { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Meeting> meetings;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public SchedulerViewModel()
+        {
+            this.Meetings = new ObservableCollection<Meeting>();
+            this.AddAppointments();
+        }
+        public ObservableCollection<Meeting> Meetings
+        {
+            get
+            {
+                return this.meetings;
+            }
+            set
+            {
+                this.meetings = value;
+                this.RaiseOnPropertyChanged("Meetings");
+            }
+        }
+        private void AddAppointments()
+        {
+            var meeting = new Meeting()
+            {
+                From = new DateTime(2020, 07, 05, 10, 0, 0),
+                To = new DateTime(2020, 07, 05, 11, 0, 0),
+                EventName = "Occurs Daily",
+                BackgroundColor = new SolidColorBrush(Colors.LightSeaGreen),
+                ForegroundColor = new SolidColorBrush(Colors.White),
+                Id = 1
+            };
+            var exceptionDate = new DateTime(2020, 07, 09);
+            meeting.RecurrenceRule = "FREQ=DAILY;COUNT=20";
+            meeting.RecurrenceExceptions = new ObservableCollection<DateTime>()
+            {
+              exceptionDate
+            };
+            this.Meetings.Add(meeting);
+        }
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+    
+
